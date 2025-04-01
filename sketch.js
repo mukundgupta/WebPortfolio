56172b50701a64ab9f37d0ad4cbc2f0187634aae
mukundgupta <mukundgupta2121@gmail.com>
2025-04-02 00:06:33 +0530

    fixed code structure, changed UI almoost completely

---
+++ sketch.js
@@ -0,0 +1,67 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const fadeIn = document.querySelectorAll('.fade-in');
+fadeIn.forEach((el) => observer.observe(el));
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const interBubble = document.querySelector('.interactive');
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+  
+    const move = () => {
+        curX += (tgX - curX) / 20;
+        curY += (tgY - curY) / 20;
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(move);
+    };
+  
+    window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+    });
+  
+    move();
+  });
+
+// Hide #mainHeading on scroll
+window.addEventListener('scroll', () => {
+  const mainHeading = document.getElementById('mainHeading');
+  const scrollPosition = window.scrollY;
+
+  // Adjust the opacity based on scroll position
+  if (scrollPosition > 200) {
+    mainHeading.style.opacity = '0'; // Hide the heading
+    mainHeading.style.transition = 'opacity 0.7s ease'; // Smooth transition
+  } else {
+    mainHeading.style.opacity = '1'; // Show the heading
+  }
+
+
+  const hello = document.getElementById('hello-heading');
+    // Adjust the opacity based on scroll position
+    if (scrollPosition > 190) {
+      hello.style.opacity = '1'; // Hide the heading
+      hello.style.transition = 'opacity 0.9s ease'; // Smooth transition
+    } else {
+      hello.style.opacity = '0'; // Show the heading
+    }
+});
+
+
+
+
+
